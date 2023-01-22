cabf53ecb58b7fa00ff7e3d4359b382719927bfe
Tharaka Ariyarathna <tharaka.ariyarathna@velaris.io>
2023-01-22 14:01:42 +0530

    Created api to update task

--- Routes/Controllers/taskController.js
+++ Routes/Controllers/taskController.js
@@ -5,12 +5,12 @@ export const createTask = (req, res) => {
   const { title, description } = req.body;
   if (title && description) {
     const task = {
-        title: title,
-        description : description,
-        id : taskList.length + 1
-    }
-    taskList.push(task) ;
-    return res.status(200).json("Task added successfully") ;
+      title: title,
+      description: description,
+      id: taskList.length + 1,
+    };
+    taskList.push(task);
+    return res.status(200).json("Task added successfully");
   } else {
     return res.status(400).json("Incomplete request");
   }
@@ -18,91 +18,39 @@ export const createTask = (req, res) => {
 
 //get tasks
 export const getTasks = (req, res) => {
-    if(taskList.length > 0){
-        return res.status(200).json({taskList : taskList}) ;
-    }else{
-        return res.status(404).json("No task to display") ;
-    }
-}
+  if (taskList.length > 0) {
+    return res.status(200).json({ taskList: taskList });
+  } else {
+    return res.status(404).json("No task to display");
+  }
+};
 
 //get specific task
 export const getTask = (req, res) => {
-    const id = req.params.id ;
-    const task = taskList.find((task) => task.id == id) ;
-    if(task){
-        return res.status(200).json({task : task}) ;
-    }else{
-        return res.status(404).json("Task not found") ;
+  const id = req.params.id;
+  const task = taskList.find((task) => task.id == id);
+  if (task) {
+    return res.status(200).json({ task: task });
+  } else {
+    return res.status(404).json("Task not found");
+  }
+};
+
+//edit a task
+export const editTask = (req, res) => {
+  const id = req.params.id;
+  const { title, description } = req.body;
+  const task = taskList.find((task) => task.id == id);
+  if (task) {
+    if (title) {
+      task.title = title;
     }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    if (description) {
+      task.description = description;
+    }
+    console.log(taskList[id]) ;
+    return res.status(200).json("Task updated successfully");
+  } else {
+    return res.status(404).json("Task not found");
+  }
+};
--- Routes/taskRoutes.js
+++ Routes/taskRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express' ;
-import { createTask, getTasks, getTask } from './Controllers/taskController.js';
+import { createTask, getTasks, getTask, editTask } from './Controllers/taskController.js';
 
 const router = express.Router() ;
 
 router.post("/", createTask) ;
 router.get("/", getTasks) ;
 router.get("/:id", getTask) ;
+router.put("/:id", editTask) ;
 
 export default router ;
