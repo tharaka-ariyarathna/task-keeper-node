93ab86601c7531fd42316b66dc99d7e1fa9067ae
Tharaka Ariyarathna <tharaka.ariyarathna@velaris.io>
2023-01-23 23:47:21 +0530

    Changed requestValidation

--- Routes/taskRoutes.js
+++ Routes/taskRoutes.js
@@ -4,15 +4,15 @@ import {
   getTasks,
   getTask,
   editTask,
-  deleteTask,
+  deleteTask
 } from "../Controllers/taskController.js";
-import { createTaskValidation, taskValidation } from "../middleware/requestValidation.js";
+import { createTaskValidation, taskValidation, test } from "../middleware/requestValidation.js";
 
 const router = express.Router();
 
 router.post("/", createTaskValidation, createTask);
 router.get("/", getTasks);
-router.get("/:id", taskValidation, getTask);
+router.get("/:id", taskValidation, test, getTask);
 router.put("/:id", taskValidation, editTask);
 router.delete("/:id", taskValidation, deleteTask);
 
